ef45e50df573d4ae55a516a0d10e391f2289add5
Grzegorz Kolek <gkolek@gazeta.pl>
2019-02-16 13:53:46 +0100

    5th - messssss!

--- script.js
+++ script.js
@@ -12,8 +12,19 @@ class Users {
         this.fetchUsers();
         this.render();
     }
+
+    mypromise() {
+        cosnt inStorehouse = true;
+        const menu = ['sandwich', 'soup', 'drink];
+const order = 'soup'
+
+        const getMyOrder = new Promise((resolve, reject => {
+
+        }))
+    }
+
     fetchUsers() {
-        fetch('https://randomuser.me/api/?results=5&gender=female')
+        fetch('https://randomuser.me/api/?results= 5&gender=female')
             .then(response => {
                 console.log('mam odpowiedź')
                 console.log(response)
@@ -23,22 +34,25 @@ class Users {
                 this.users = data
                 this.render();
             })
-            .catch(error=>{
+            .catch(error => {
                 console.warn('mam błąd :(')
             })
 
     }
     render() {
-        const ul = document.createElement('ul');
+        const row = document.createElement('ul');
         if (!this.users.results) {
             return
         }
         this.users.results.forEach(elem => {
-            const li = document.createElement('li');
-            li.innerHTML = `${elem.name.first} ${elem.name.last}`;
-            ul.appendChild(li);
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <h2>${elem.name.first} ${elem.name.last}</h2>
+            <img src = ${elem.picture.thumbnail} ${elem.cell}/>
+            <hr/>`
+            row.appendChild(div);
         });
-        this.container.appendChild(ul);
+        this.container.appendChild(row);
     }
 }
 
